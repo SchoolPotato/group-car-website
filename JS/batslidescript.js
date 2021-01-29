5aafa95601d0338157402e6e4b8ce12d8503d7ec
chazcoffdude <71471786+chazcoffdude@users.noreply.github.com>
2021-01-29 16:59:35 -0700

    Added script for slideshow @Index

---
+++ JS/batslidescript.js
@@ -0,0 +1,60 @@
+
+
+itemlist = [
+
+"images/batslide1.jpg",
+"images/batslide2.jpg",
+"images/batslide3.jpg",
+"images/batslide4.jpg",
+
+]
+//add any image file paths above
+
+//vars
+
+let listpos = 0;
+document.getElementById("lbutton").addEventListener("click", lclick);
+
+document.getElementById("rbutton").addEventListener("click", rclick);
+
+let displayimg = document.getElementById("test-image")
+
+
+//func
+
+function rclick(){
+console.log("Went through.")
+if(listpos >= itemlist.length - 1){
+    listpos = 0;
+    updatelist()
+}
+else{
+    listpos++;
+    updatelist()
+}
+}
+
+function lclick(){
+    console.log("Went through.")
+
+    if(listpos <= 0){
+        listpos = itemlist.length - 1; 
+        updatelist()
+    }
+    else{
+        listpos--;
+        updatelist()
+    }
+}
+
+
+
+//function for changing image
+
+function updatelist(){
+
+    console.log(listpos);
+    console.log(itemlist[listpos]);
+displayimg.src = itemlist[listpos];
+
+}
